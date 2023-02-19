9757368e958af58560369e5a759ed83b1c52d690
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-19 13:36:41 -0600

    created confirm prompt for submission

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import { useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import understanding from '../Understanding/Understanding';
 
 const results = () => {
     const history = useHistory();
 
+    // global variables from store
     const feelings = useSelector(store => store.feelings);
     const understanding = useSelector(store => store.understanding);
     const support = useSelector(store => store.support);
     const comments = useSelector(store => store.comments);
 
-
+    // submit
     const handleClick = () =>   {
-        alert('submission has been made!');
-        history.push('/')
-    }
+        if(confirm('ok to submit?')) {
+            history.push('/')
+        }
+        }
 
     return (
     <div>
-    <h1>Results</h1>
-    <p><b>feelings</b>: {feelings}</p>
-    <p><b>understanding</b>: {understanding}</p>
-    <p><b>support</b>: {support}</p>
-    <p><b>comments</b>: {comments}</p>
-    <button onClick={handleClick}>submit</button>
+        <h1>Results</h1>
+            <p><b>feelings</b>: {feelings}</p>
+            <p><b>understanding</b>: {understanding}</p>
+            <p><b>support</b>: {support}</p>
+            <p><b>comments</b>: {comments}</p>
+        <button onClick={handleClick}>submit</button>
     </div>
     )
 }
