31ffbd2bb7a74bcaa76dba4adeef4662d8ba1ff7
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-19 12:38:11 -0600

    removed code comments. created results component

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+
 
 const results = () => {
-    const history = useHistory();
 
-    const results = useSelector(store => store.results);
-    const dispatch = useDispatch();
-    
-    const handleChange = () => {
-        dispatch({type: 'SET_RESULTS', payload: event.target.value});
-    }
 
     return (
     <div>
     <h1>Results</h1>
-    <p>Review Results</p>
-    <input value={results} onChange={handleChange} type="number" min={0} max={5}/>
     <button>submit</button>
-    {/* <div>
-        <input value={results} onChange={handleChange} className="input" type="text" />
-        <button onClick={() => history.push('/resultsPage')} className="button">Next</button>
-    </div> */}
     </div>
     )
 }
